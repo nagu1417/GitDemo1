ecd5875ca92bf8b549ec6eb4017463bf5756fe93
unknown <nagesh1417@gmail.com>
2019-02-10 19:40:20 +0530

    first commit

---
+++ src/TestPackage/PracticeApp.java
@@ -0,0 +1,70 @@
+package TestPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PracticeApp {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver","F://Selenium downloads//chromedriver_win32//chromedriver.exe"); 
+		WebDriver driver=new ChromeDriver();    // this line launch chrome browser
+		driver.get("http://toolsqa.com/automation-practice-form/");
+		driver.manage().window().maximize();
+		try {
+			
+		 Thread.sleep(1000);                 //1000 milliseconds is one second.
+		} 
+		catch(InterruptedException ex) {
+		   Thread.currentThread().interrupt();
+		}
+
+		//Thread.sleep(2000);
+		//think(3);
+	/*	driver.findElement(By.name("firstname")).sendKeys("Nagesh");
+		driver.findElement(By.name("lastname")).sendKeys("Golakoti");
+		//selecting sex
+		driver.findElement(By.xpath("//*[@id='sex-0']")).click();
+		//selecting years of experience
+		driver.findElement(By.xpath("//*[@id='exp-2']")).click();
+		//setting date
+		driver.findElement(By.xpath("//*[@id='datepicker']")).sendKeys("03-05-2017");
+		*/
+		
+		
+		//selecting multiple checkboxes
+		//WebElement Checkbox1=driver.findElement(By.xpath("//*[@id'profession-0']"));
+		//Got Invalid selector issue 
+		
+		/*
+		WebElement Checkbox1=driver.findElement(By.cssSelector("#profession-0"));
+		Checkbox1.click();
+		System.out.println("Selected check box 1");
+		WebElement Checkbox2=driver.findElement(By.cssSelector("#profession-1"));
+		Checkbox2.click();
+		System.out.println("Selected check box 2");*/
+		
+		
+		/*WebElement we=driver.findElement(By.xpath("//*[@id='photo']"));
+		we.click();
+		//Runtime.getRuntime().exec("Script.exe");
+		we.sendKeys("C:\\Users\\Nagesh\\Desktop\\New Text Document.txt");
+		*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//driver.close();
+	}
+
+}
---
+++ src/TestPackage/Test1.java
@@ -0,0 +1,28 @@
+package TestPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","F://Selenium downloads//chromedriver_win32//chromedriver.exe"); 
+		WebDriver driver=new ChromeDriver();    // this line launch chrome browser
+		driver.get("http://www.google.co.in");
+		
+		
+		//F:\Selenium downloads\chromedriver_win32
+		
+		/*System.setProperty("webdriver.gecko.driver","F://Selenium downloads//geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();*/
+		driver.get("http://www.google.com");
+		System.out.println("Url opened succesfully");
+		String s=driver.getCurrentUrl();
+		System.out.println("Page url:"+s);
+
+	}
+
+}
